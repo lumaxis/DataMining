c5859031871953bc5d8a397c79f216546e80a9ab
Lukas Spieß <lukas.spiess@gmail.com>
2013-10-21 12:19:44 +0200

    Versuch 1, 2.1

--- appendGeoCordinates.py
+++ appendGeoCordinates.py
@@ -1,7 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct 21 10:23:25 2013
+Spyder Editor
 
-@author: mathis
+This temporary script file is located here:
+C:\Users\lukas\.spyder2\.temp.py
 """
+import time
+import urllib, json
+import pandas as pd
+import numpy as np
+randn = np.random.randn
 
+df = pd.read_csv("C:\Users\lukas\Desktop\EnergyMix.csv", index_col=0)
+graph = df[0:4]
+#graph.plot(kind='bar')
+
+#Beispieldaten statt Google Maps
+gmaps = pd.DataFrame(randn(len(df.index),2), columns=('Lat','Long'), index=df.index)
+def geocode(addr):
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false" %   (urllib.quote(addr.replace(' ', '+')))
+    data = urllib.urlopen(url).read()
+    info = json.loads(data).get("results")[0].get("geometry").get("location")  
+    return info
+    
+for row_index, row in df.iterrows():
+    res = (geocode(str(row_index)))
+    gmaps.loc[row_index, ['Lat']] = res['lat']
+    gmaps.loc[row_index, ['Long']] = res['lng']
+    time.sleep(1)
+
+#Geokoordinaten and Dataframe anhängen
+df = df.append(gmaps)
+df.to_csv("C:\Users\lukas\Desktop\EnergyMixGeo.csv")
