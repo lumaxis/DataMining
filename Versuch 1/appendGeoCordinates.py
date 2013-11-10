365ee0a371fabb3445006deba854ce1cf08ee9f7
Lukas Spieß <lukas.spiess@gmail.com>
2013-11-10 16:38:48 +0100

    adds simplified graphs for energy sorts

--- Versuch 1/appendGeoCordinates.py
+++ Versuch 1/appendGeoCordinates.py
@@ -12,28 +12,50 @@ import numpy as np
 randn = np.random.randn
 
 df = pd.read_csv("C:\Users\lukas\Desktop\EnergyMix.csv", index_col=0)
-df.plot()
 
-#Simplified graph
-#graph = df[0:4]
-#graph.plot()
+max_idx = 7
+col = 0
 
-#Random example data instead of Google Maps
-gmaps = pd.DataFrame(randn(len(df.index),2), columns=('Lat','Long'), index=df.index)
-def geocode(addr):
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false" %   (urllib.quote(addr.replace(' ', '+')))
-    data = urllib.urlopen(url).read()
-    info = json.loads(data).get("results")[0].get("geometry").get("location")  
-    return info
-        
-df['Lat']= pd.Series(0.0, index=df.index)
-df['Long']= pd.Series(0.0, index=df.index)
+#Simplified graphs for every energy sort
+df_sort = df.sort(['Oil'], ascending=False)
+graph1 = df_sort.iloc[0:max_idx,0:1]
+graph1.plot()
 
-for row_index, row in df.iterrows():
-    res = (geocode(str(row_index)))
-    df.loc[row_index, ['Lat']] = res['lat']
-    df.loc[row_index, ['Long']] = res['lng']
-    time.sleep(1)
+df_sort = df.sort(['Gas'], ascending=False)
+df_gas = df_sort.iloc[0:max_idx,1:2]
+df_gas.plot()
 
-#Append geodata to dataframe
-df.to_csv("C:\Users\lukas\Desktop\EnergyMixGeo.csv")
+df_sort = df.sort(['Coal'], ascending=False)
+graph3 = df_sort.iloc[0:max_idx,2:3]
+graph3.plot()
+
+df_sort = df.sort(['Nuclear'], ascending=False)
+graph4 = df_sort.iloc[0:max_idx,3:4]
+graph4.plot()
+ 
+df_sort = df.sort(['Hydro'], ascending=False)
+graph5 = df_sort.iloc[0:max_idx,4:5]
+graph5.plot()
+
+
+#==============================================================================
+# #Random example data instead of Google Maps
+# gmaps = pd.DataFrame(randn(len(df.index),2), columns=('Lat','Long'), index=df.index)
+# def geocode(addr):
+#     url = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false" %   (urllib.quote(addr.replace(' ', '+')))
+#     data = urllib.urlopen(url).read()
+#     info = json.loads(data).get("results")[0].get("geometry").get("location")  
+#     return info
+#         
+# df['Lat']= pd.Series(0.0, index=df.index)
+# df['Long']= pd.Series(0.0, index=df.index)
+# 
+# for row_index, row in df.iterrows():
+#     res = (geocode(str(row_index)))
+#     df.loc[row_index, ['Lat']] = res['lat']
+#     df.loc[row_index, ['Long']] = res['lng']
+#     time.sleep(1)
+# 
+# #Append geodata to dataframe
+# df.to_csv("C:\Users\lukas\Desktop\EnergyMixGeo.csv")
+#==============================================================================
