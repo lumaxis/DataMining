fece814a7007e53a033954bbc1b63ca44504e2bd
Lukas Spieß <lukas.spiess@gmail.com>
2013-11-18 11:10:27 +0100

    clean up merge

--- Versuch 4/docclass.py
+++ Versuch 4/docclass.py
@@ -34,41 +34,42 @@ class Classifier:
 
     def incf(self,f,cat):
         if self.fc.has_key(f) == False:
-    
-    '''
-    calculates the probability that a given word f belongs to category cat
-    '''
             self.fc[f] = {'good':0, 'bad':0}
         self.fc[f][cat] =+ 1
 
     def incc(self,cat):
         self.cc[cat] =+ 1
+
+    '''
+    calculates the probability that a given word f belongs to category cat
+    '''
     def fprob(self,f,cat):
         return self.fc[f][cat]/self.cc[cat]
 
     def fcount(self,f,cat):
         return self.fc[f][cat]
-    
-    '''
+
 
     def catcount(self, cat):
-    calculates the same propability as fprob but corrects extrem values that
-    may occur if a word does not occur often in training data
         return self.cc[cat]
 
     def totalcount(self):
-    '''
         return sum(self.cc.values())
 
     def train(self,item,cat):
         features = self.getfeatures(item)
-    def weightedprob(self,f,cat):
-        # the probability to return if f did not occur yet in training-data
         for word in features:
             self.incf(self,word,cat)
-        initprob = 0.5
         self.incc(self,cat)
-        
+
+
+    '''
+    calculates the same propability as fprob but corrects extrem values that
+    may occur if a word does not occur often in training data'''
+    def weightedprob(self,f,cat):
+        # the probability to return if f did not occur yet in training-data
+
+        initprob = 0.5
         cnt = self.fc[f]['good']+self.fc[f]['bad']
         return (initprob+cnt*self.fprob(f,cat))/(1+cnt)
 
