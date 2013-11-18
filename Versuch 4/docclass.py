bdda08dd0be50a0cf9c0c3cb24ab6286a987b32c
Mathis Hoffmann <mathis@hoffpost.de>
2013-11-18 10:19:07 +0100

    adds Classifier-class (body and constructur)

---
+++ Versuch 4/docclass.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 10:13:25 2013
+
+@author: Mathis
+"""
+
+class Classifier:
+    
+    fc = {}
+    
+    cc = {}
+    
+    def __init__ ():
+        fc = {}
+        cc = {'good':0, 'bad':0}
