d89100bebc1ac4736520cf0fa748bcabb6a40a1e
Mathis Hoffmann <mathis@hoffpost.de>
2013-11-18 10:29:36 +0100

    fixes constructor

--- Versuch 4/docclass.py
+++ Versuch 4/docclass.py
@@ -7,10 +7,29 @@ Created on Mon Nov 18 10:13:25 2013
 
 class Classifier:
     
+    '''
+    a dictionary like this:
+    {
+        'wort1': {'good': x, 'bad': y}
+        'wort2': {'good': z, 'bad': a}
+        ...
+        'wortn': {'good': b, 'bad': c}
+    }
+    '''
     fc = {}
     
+    '''
+    a dictionary that counts the number documents in both categories
+    '''
     cc = {}
     
-    def __init__ ():
-        fc = {}
-        cc = {'good':0, 'bad':0}
+    def __init__ (self):
+        self.fc = {}
+        self.cc = {'good':0, 'bad':0}
+    
+    '''
+    calculates the propability that a given word f belongs to category cat
+    '''
+    def fprob(self,f,cat):
+        
+        
