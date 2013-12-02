923ba57207426bffa577bb2987f4eccf0553f275
Lukas Spieß <lukas.spiess@gmail.com>
2013-12-02 13:16:37 +0100

    implement FaceRecognition

--- Versuch 6/faceRecognitionTemplate.py
+++ Versuch 6/faceRecognitionTemplate.py
@@ -2,8 +2,8 @@ from os.path import isdir,join,normpath
 from os import listdir
 
 from PIL import Image
-from numpy import asfarray,dot,argmin,zeros
-from numpy import average,sort,trace
+from numpy import asfarray,dot,argmin,zeros, amax, transpose, apply_along_axis
+from numpy import average,sort,argsort,trace
 from numpy.linalg import svd,eigh
 from numpy import concatenate, reshape
 from math import sqrt
@@ -34,19 +34,65 @@ def generateListOfImgs(listOfTrainFiles):
     l = []
     for f in listOfTrainFiles:
         img = Image.open(f)
+        img = img.convert("L")
         l.append(img)
-    return l        
-    
+    return l
+
+def convertImgListToNumpyData(imgList):
+    dataList = []
+    for img in imgList:
+        data = asfarray(img.getdata())
+        dataMax = amax(data)
+        dataList.append(data/dataMax)
+    return asfarray(dataList)
+
+def calculateEigenfaces(adjfaces):
+    X = transpose(adjfaces)
+    #CV = dot(X,adjfaces)
+
+    #Calculate Eigenvalues and simplified Eigenvectors
+    w,v = eigh(dot(adjfaces,X))
+
+    #Multiply original matrix with simplified Eigenvectors to get real Eigenvectors
+    v = dot(X,v)
+
+    #Sort Eigenvalues to get indices for selection of relevant Eigenvectors
+    idx = w.argsort()[::-1]
+
+    #Order Eigenvalues by indices?
+    w = w[idx]
+
+    #Order Eigenvectors by indices
+    v = v[:,idx]
+    return v
+
+def reshapeVectorToImage(x):
+    global width,height
+    print x.shape
+    print width*height
+    return x.reshape(height,width)
+
 ####################################################################################
 #Start of main programm
 
-#Choose Directory which contains all training images 
+
+#Choose Directory which contains all training images
 TrainDir=tkFileDialog.askdirectory(title="Choose Directory of training images")
 #Choose the file extension of the image files
 Extension='png' 
 #Choose the image which shall be recognized
 #testImageDirAndFilename=tkFileDialog.askopenfilename(title="Choose Image to detect")
-generateListOfImgs(parseDirectory(TrainDir,Extension))
-
+imgList = generateListOfImgs(parseDirectory(TrainDir,Extension))
+width,height = imgList[0].size
 ####################################################################################
 # Implement required functionality of the main programm here
+
+a = convertImgListToNumpyData(imgList)
+avg = average(a,axis=0)
+
+NormedArrayOfFaces = a - avg
+
+faces = calculateEigenfaces(NormedArrayOfFaces)
+relevantFaces = faces[:,:6]
+
+apply_along_axis(reshapeVectorToImage,axis=0,arr = relevantFaces)
